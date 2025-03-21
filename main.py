4723dbe29c33db3050022a92415abcc28800cce8
Maple778 <134897422+Maple778@users.noreply.github.com>
2025-03-21 12:02:15 +0800

    impl: plot scenario on borregas ave (#1)

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-
+import matplotlib
+matplotlib.use("TkAgg")
+import matplotlib.pyplot as plt
 from baidu_apollo_plot import plot_scenario
 
 plot_scenario(
-    "testdata/scenario_0.resim_1.00000",
+    "testdata/gen_0_sce_6.00000",
     "data/maps/borregas_ave/base_map.bin",
     "out"
 )
-
